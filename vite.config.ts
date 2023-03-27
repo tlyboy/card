9f053abef0117fd713a041a1b54604205a3efb43
Guany <1084072690@qq.com>
2023-03-27 11:28:04 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     }),
     Pages(),
     AutoImport({
-      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core']
+      imports: ['vue', 'vue-router', '@vueuse/core']
     }),
     Components()
   ],
